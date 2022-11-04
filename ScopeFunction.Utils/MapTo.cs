c20b59fbac8b58dbc931d205d983a7e89c2c7eb6
Caybo Kotzé <caybokotze@gmail.com>
2022-11-04 17:51:31 +0100

    if mapping source is null return null

--- ScopeFunction.Utils/MapTo.cs
+++ ScopeFunction.Utils/MapTo.cs
@@ -5,9 +5,31 @@ namespace ScopeFunction.Utils
 {
     public static class AutoMap
     {
+        public static TDestination MapTo<TDestination>(this object source) where TDestination : class, new()
+        {
+            if (source is null)
+            {
+                return null;
+            }
+            
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap(source.GetType(), typeof(TDestination));
+            });
+
+            var mapper = new Mapper(config);
+
+            return mapper.Map(source, new TDestination());
+        }
+        
         public static TDestination MapTo<TSource, TDestination>(this TSource objectSource)
             where TSource : class where TDestination : class
         {
+            if (objectSource is null)
+            {
+                return null;
+            }
+            
             var config = new MapperConfiguration(cfg => { cfg.CreateMap<TSource, TDestination>(); });
 
             var mapper = new Mapper(config);
@@ -18,6 +40,11 @@ namespace ScopeFunction.Utils
         public static TDestination MapTo<TSource, TDestination>(this TSource objectSource, TDestination destination)
             where TSource : class where TDestination : class
         {
+            if (objectSource is null)
+            {
+                return null;
+            }
+            
             var config = new MapperConfiguration(cfg => { cfg.CreateMap<TSource, TDestination>(); });
 
             var mapper = new Mapper(config);
@@ -28,6 +55,11 @@ namespace ScopeFunction.Utils
         public static TDestination MapTo<TSource, TDestination>(this TSource objectSource, TDestination destination,
             Action<TSource> overrideSource) where TSource : class where TDestination : class
         {
+            if (objectSource is null)
+            {
+                return null;
+            }
+            
             var config = new MapperConfiguration(cfg => { cfg.CreateMap<TSource, TDestination>(); });
 
             var mapper = new Mapper(config);
